c9af54d3d4d9606c6d9d6eef27a9cedd0356b61d
j1mmyzz <jime82075@gmail.com>
2023-05-29 02:10:39 -0400

    wrap

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,29 +1,21 @@
 import type { NextPage } from "next";
 import Layout from "@/components/layout";
-import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
 
 import {
   Text,
   Flex,
   Card,
-  CardHeader,
   CardBody,
   CardFooter,
   Button,
-  Checkbox,
-  CheckboxGroup,
-  Input,
   Heading,
   Stack,
-  Divider,
-  Box,
   ButtonGroup,
   Image,
+  Wrap,
+  WrapItem,
 } from "@chakra-ui/react";
 import NextLink from "next/link";
-
-import Link from "next/link";
 
 const GroceryList: NextPage = () => {
   return (
@@ -37,66 +29,76 @@ const GroceryList: NextPage = () => {
         <Text mx={10} my={10} fontSize="50">
           <strong>Projects</strong>
         </Text>
-        <Flex justifyContent={"space-evenly"}>
-          <Card maxW="sm" mx={5}>
-            <CardBody>
-              <Image
-                src="/Screenshot 2023-05-29 at 1.01.07 AM.png"
-                alt="Green double couch with wooden legs"
-                borderRadius="lg"
-              />
-              <Stack mt="6" spacing="3">
-                <Heading size="md">Maze Algorithm GUI</Heading>
-                <Text>
-                  My AP CSA Final Project. It's a Java program that generates a
-                  maze and finds the optimal path to solve it. You also have the
-                  option to create you own maze.
-                </Text>
-              </Stack>
-            </CardBody>
-            <CardFooter>
-              <ButtonGroup spacing="2">
-                <NextLink
-                  href="/https://github.com/j1mmyzz/MazeAlgorithm"
-                  passHref
-                >
-                  <Button variant="solid" colorScheme="blue">
-                    View Source Code
-                  </Button>
-                </NextLink>
-              </ButtonGroup>
-            </CardFooter>
-          </Card>
-          <Card maxW="sm" mx={5}>
-            <CardBody>
-              <Image
-                src="/Screenshot 2023-05-29 at 1.00.34 AM.png"
-                alt="empire game"
-                borderRadius="lg"
-              />
-              <Stack mt="6" spacing="3">
-                <Heading size="md">Empire CyberSecurity Game</Heading>
-                <Text>
-                  A cybersecurity web app game made in HTML, CSS, JS. I made
-                  this as an intern at ETA media. It's designed to simulate
-                  real-life cyber threats and teaches basic cybersecurity.
-                </Text>
-              </Stack>
-            </CardBody>
-            <CardFooter>
-              <ButtonGroup spacing="2">
-                <NextLink
-                  href="/https://github.com/j1mmyzz/cybersecurity-game"
-                  passHref
-                >
-                  <Button variant="solid" colorScheme="blue">
-                    View Source Code
-                  </Button>
-                </NextLink>
-              </ButtonGroup>
-            </CardFooter>
-          </Card>
-        </Flex>
+        <Wrap
+          spacing="6"
+          justify="center"
+          align="center"
+          mt="8"
+          maxW={{ base: "100%", sm: "80%" }}
+        >
+          <WrapItem>
+            <Card maxW="sm">
+              <CardBody>
+                <Image
+                  src="/Screenshot 2023-05-29 at 1.01.07 AM.png"
+                  alt="Green double couch with wooden legs"
+                  borderRadius="lg"
+                />
+                <Stack mt="6" spacing="3">
+                  <Heading size="md">Maze Algorithm GUI</Heading>
+                  <Text>
+                    My AP CSA Final Project. It's a Java program that generates
+                    a maze and finds the optimal path to solve it. You also have
+                    the option to create your own maze.
+                  </Text>
+                </Stack>
+              </CardBody>
+              <CardFooter>
+                <ButtonGroup spacing="2">
+                  <NextLink
+                    href="https://github.com/j1mmyzz/MazeAlgorithm"
+                    passHref
+                  >
+                    <Button as="a" variant="solid" colorScheme="blue">
+                      View Source Code
+                    </Button>
+                  </NextLink>
+                </ButtonGroup>
+              </CardFooter>
+            </Card>
+          </WrapItem>
+          <WrapItem>
+            <Card maxW="sm">
+              <CardBody>
+                <Image
+                  src="/Screenshot 2023-05-29 at 1.00.34 AM.png"
+                  alt="empire game"
+                  borderRadius="lg"
+                />
+                <Stack mt="6" spacing="3">
+                  <Heading size="md">Empire CyberSecurity Game</Heading>
+                  <Text>
+                    A cybersecurity web app game made in HTML, CSS, JS. I made
+                    this as an intern at ETA media. It's designed to simulate
+                    real-life cyber threats and teaches basic cybersecurity.
+                  </Text>
+                </Stack>
+              </CardBody>
+              <CardFooter>
+                <ButtonGroup spacing="2">
+                  <NextLink
+                    href="https://github.com/j1mmyzz/cybersecurity-game"
+                    passHref
+                  >
+                    <Button as="a" variant="solid" colorScheme="blue">
+                      View Source Code
+                    </Button>
+                  </NextLink>
+                </ButtonGroup>
+              </CardFooter>
+            </Card>
+          </WrapItem>
+        </Wrap>
       </Flex>
     </Layout>
   );
