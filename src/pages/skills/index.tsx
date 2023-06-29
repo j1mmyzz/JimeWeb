35d826e7bc044cb7a800ef766529029d373068bd
j1mmyzz <jime82075@gmail.com>
2023-06-29 18:18:07 -0400

    icons

--- src/pages/skills/index.tsx
+++ src/pages/skills/index.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from "next";
-import { Text, Image, Center } from "@chakra-ui/react";
+import { Text, Image, Center, WrapItem, Wrap, Box } from "@chakra-ui/react";
 import Layout from "@/components/layout";
 import Footer from "@/components/footer";
 const Skills: NextPage = () => {
@@ -12,13 +12,35 @@ const Skills: NextPage = () => {
         Languages{" "}
       </Text>
       <Center>
-        <Image src="java.png/" alt="Java" boxSize="64px" mx={5} />
-        <Image src="python.png/" alt="python" boxSize="64px" mx={5} />
-        <Image src="javascript.png/" alt="JavaScript" boxSize="64px" mx={5} />
-        <Image src="typescript.png/" alt="Java" boxSize="64px" mx={5} />
-        <Image src="html.png/" alt="HTML" boxSize="64px" mx={5} />
-        <Image src="css.png/" alt="CSS" boxSize="64px" mx={5} />
+        <Box display="flex" alignItems="center">
+          <Wrap>
+            <WrapItem>
+              <Image src="java.png/" alt="Java" boxSize="64px" mx={3} />
+            </WrapItem>
+            <WrapItem>
+              <Image src="python.png/" alt="python" boxSize="64px" mx={3} />
+            </WrapItem>
+            <WrapItem>
+              <Image
+                src="javascript.png/"
+                alt="JavaScript"
+                boxSize="64px"
+                mx={5}
+              />
+            </WrapItem>
+            <WrapItem>
+              <Image src="typescript.png/" alt="Java" boxSize="60px" mx={3} />
+            </WrapItem>
+            <WrapItem>
+              <Image src="html.png/" alt="HTML" boxSize="64px" mx={3} />
+            </WrapItem>
+            <WrapItem>
+              <Image src="css.png/" alt="CSS" boxSize="67px" mx={3} />
+            </WrapItem>
+          </Wrap>
+        </Box>
       </Center>
+
       <Text my={2} fontSize={40} textAlign="center">
         Tools{" "}
       </Text>
